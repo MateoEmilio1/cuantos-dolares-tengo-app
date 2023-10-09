9d6a846895aafd7f341277d913819e40f9f1f82f
Tute <mateoyastor60@gmail.com>
2023-10-09 17:27:31 -0300

    acc new

--- src/app/Calculadora.tsx
+++ src/app/Calculadora.tsx
@@ -28,9 +28,16 @@ export default function Calculadora() {
         {/* Ingreso cantidad de pesos */}
         <label htmlFor="cant">Cantidad de pesos: </label>
         <input type="text" required autoComplete="off" id="cant" />
-        {/* Ingreso el tipo de Dolar */}      
-        <label htmlFor="tipoDolar">Tipo de Dólar: </label>
-        <input type="text" required autoComplete="off" id="tipoDolar" name="tipoDolar" />
+         {/* Ingreso el tipo de Dolar */}
+         <label htmlFor="tipoDolar">Tipo de Dólar: </label>
+        <select id="tipoDolar">
+          <option value="Blue">Blue</option>
+          <option value="Oficial">Oficial</option>
+          <option value="Bolsa">Bolsa</option>
+          <option value="Contado con liquidación">Contado con liquidación</option>
+          <option value="Solidario (Turista)">Solidario (Turista)</option>
+          <option value="Mayorista">Mayorista</option>
+        </select>
         <button
         type="submit"
         className="w-24 h-10 bg-gradient-to-r from-slate-400 to-slate-600 text-black dark:text-white px-2 py-1.5 rounded-md mt-8"
